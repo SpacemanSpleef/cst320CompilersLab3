826108f3876947c25d6bdc6d926578a940ea63cc
Theodore Gwynn <theo.gwynn@loki.labs.cset.oit.edu>
2026-01-22 16:41:26 -0800

    Continued working on lab, near passing test 1

--- parse.cpp
+++ parse.cpp
@@ -1,56 +1,50 @@
-//**************************************
-// parse.cpp
+//************************************** 
 //
-// Starting point for top-down recursive-descent parser
-// Used in CST 320 Lab2
-//
+// parse.cpp 
+// Starting point for top-down recursive-descent parser 
+// Used in CST 320 Lab2 
+// 
 // Author: Phil Howard 
-// phil.howard@oit.edu
-//
+// phil.howard@oit.edu 
 
-#include <iostream>
-#include "lex.h"
-#include "parse.h"
-#include "utils.h"
-#include "tokens.h"
-
-//*******************************************
-// Find a PROG non-terminal
-// NOTE: this code is not complete
-bool FindPROG()
-{
-    if (!FindSTMTS())
-    {
-        Error("Program");
-        return false;
-    }
-    else
-    {
-        int token = PeekToken();
-        if (token != END)
-        {
-            Error("END");
-            return false;
-        }
-        AdvanceToken();
-        return true;
-    }
-
-}
-//*******************************************
-// Find a STMT non-terminal
-// NOTE: this code is not complete
-bool FindSTMT()
-{
-    if (FindEXPR())
-    {
-        int token = PeekToken();
-        if(token != ';')
-        {
-            return false;
-        }
+#include <iostream> 
+#include "lex.h" 
+#include "parse.h" 
+#include "utils.h" 
+#include "tokens.h" 
+//******************************************* 
+// // Find a PROG non-terminal /
+// / NOTE: this code is not complete 
+bool FindPROG() 
+{ 
+    if (!FindSTMTS()) 
+    { Error("Program"); return false; } 
+    else 
+    { 
+        int token = PeekToken(); 
+        if (token != END) 
+        { 
+            Error("END"); 
+            return false; 
+        } 
+        AdvanceToken(); 
+        return true; 
+    } 
+} 
+//******************************************* // Find a STMT non-terminal 
+// // NOTE: this code is not complete
+bool FindSTMT() 
+{ 
+    if (FindEXPR()) 
+    { 
+        int token = PeekToken(); 
+        if(token != ';') 
+        { 
+            return false; 
+        } 
+        AdvanceToken(); 
         std::cout << "Found a statement\n";
-        return true;
+        return true; 
     }
     else if(PeekToken() == IDENTIFIER)
     {
@@ -58,102 +52,162 @@ bool FindSTMT()
         if(PeekToken() == '=')
         {
             AdvanceToken();
-            if(FindEXPR())
+            if(FindEXPR()) 
             {
-                AdvanceToken();
-                if(PeekToken() != ';')
-                {
-                    return false;
+                 //AdvanceToken();
+                 if(PeekToken() != ';')
+                { 
+                    return false; 
                 } 
-                else
-                {
+                else 
+                { 
                     std::cout << "Found a statement\n";
-                    return true;
-                }
-            }
-        }
-    }
-    Error("Expression");
-    return false;
-
-}
-bool FindSTMTS()
-{
-    while(FindSTMT())
+                    return true; 
+                } 
+            } 
+        } 
+    } 
+    Error(";"); 
+    return false; 
+} 
+bool FindSTMTS() 
+{ 
+    while(FindSTMT()) 
     {}
-
-    return true;
+    return true; 
 }
-bool FindEXPR()
+bool FindEXPR() 
 {
     int token = PeekToken();
     if(token == '(')
     {
         AdvanceToken();
-        if(FindEXPR())
-        {
+        if(FindEXPR()) 
+        { 
             token = PeekToken();
-            if(token ==')')
-            {
-                if(FindEXPR_P())
-                {
-                    return true;
-                }
+            if(token ==')') 
+            { 
+                AdvanceToken();
+                if(FindEXPR_P()) 
+                { 
+                    return true; 
+                } 
+            } 
+        } 
+    } 
+    else if(FindTERM()) 
+    { 
+        return true; 
+    } 
+    return false; 
+} 
+bool FindEXPR_P() 
+{ 
+    if(FindPLUSOP())
+     { 
+        int token = PeekToken();
+        if(token == '(') 
+        { 
+            AdvanceToken();
+            if(FindEXPR())
+            { 
+                token = PeekToken();
+                if(token ==')')
+                { 
+                    AdvanceToken();
+                    if(FindEXPR_P()) 
+                    { 
+                        return true; 
+                    } 
+                } 
             }
-        }
-    } else if(FindTERM())
-    {
-        return true;
-    }
-    return false;
-}
-bool FindEXPR_P()
-{
-    return false;
-}
-bool FindPLUSOP()
-{
-    int token;
-    token = PeekToken();
-    if(token == '+' || token == '-')
-    {
-        AdvanceToken();
-        return true;
-    }
-    return false;
-}
-bool FindTERM()
-{
-    return true;
-}
-bool FindTERM_P()
-{
-    return false;
-}
-bool FindTIMESOP()
-{
-    int token;
-    token = PeekToken();
-    if(token == '*' || token == '/')
-    {
-        AdvanceToken();
-        return true;
-    }
-    return false;
-}
-
-/*******************************************
-bool FindExample()
-{
-    if (!FindPART1()) return false;
-    
+        } 
+    } 
+    return true; 
+} 
+bool FindPLUSOP() 
+{ 
     int token = PeekToken();
-    if (token != '+') return false;
-    AdvanceToken();         // past '+'
-
-    if (!FindPART2()) return false;
-
-    return true;
-}
-*/
-
+    if(token == '+' || token == '-') 
+    { 
+        AdvanceToken(); 
+        return true; 
+    }
+    return false; 
+} 
+bool FindTERM() 
+{ 
+    int token = PeekToken();
+    if(token == '[') 
+    { 
+        AdvanceToken();
+        if(FindEXPR()) 
+        { 
+            token = PeekToken(); 
+            if(token == ']') 
+            { 
+                AdvanceToken(); 
+                if(FindTERM_P()) 
+                { 
+                    return true; 
+                } 
+            } 
+        } 
+    } 
+    else if (token == NUM) 
+    { 
+        AdvanceToken(); 
+        return true; 
+    } else if(token == '{') 
+    { 
+        AdvanceToken(); 
+        token = PeekToken(); 
+        if(token == IDENTIFIER) 
+        { 
+            AdvanceToken(); 
+            token = PeekToken(); 
+            if(token == '}') 
+            { 
+                AdvanceToken(); 
+                return true; 
+            } 
+        } 
+    } 
+    return false; 
+} 
+bool FindTERM_P() 
+{ 
+    if(FindTIMESOP()) 
+    { 
+        int token = PeekToken();
+        if(token == '[') 
+        { 
+            AdvanceToken(); 
+            if(FindEXPR()) 
+            { 
+                token = PeekToken(); 
+                if(token ==']') 
+                { 
+                    AdvanceToken(); 
+                    if(FindTERM_P()) 
+                    { 
+                        return true; 
+                    } 
+                } 
+            } 
+        } 
+    } 
+    return true; 
+} 
+bool FindTIMESOP() 
+{ 
+    int token;
+    token = PeekToken(); 
+    if(token == '*' || token == '/') 
+    { 
+        AdvanceToken(); 
+        return true; 
+    } 
+    return false; 
+} 
+// /******************************************* bool FindExample() { if (!FindPART1()) return false; int token = PeekToken(); if (token != '+') return false; AdvanceToken(); // past '+' if (!FindPART2()) return false; return true; } */
